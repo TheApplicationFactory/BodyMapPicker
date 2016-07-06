44c3e1d4a8806012187fff1dbf2a15739197d82a
GJNilsen <gj.nilsen@yuppielabel.com>
2016-07-06 09:17:02 +0200

    Initial Commit
    Added Example Project

---
+++ BodyMapPickerExample/BodyMapPickerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyMapPickerExample
+//
+//  Created by Geert-Jan Nilsen on 06/07/16.
+//  Copyright © 2016 Application Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
